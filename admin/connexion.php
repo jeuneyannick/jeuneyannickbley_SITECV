c67b1874eb04e2b6b21cd58201f70432f302487b
jeune yannick <yannick.bley@lepoles.com>
2017-11-29 16:56:38 +0100

    commit responsive

--- admin/connexion.php
+++ admin/connexion.php
@@ -11,7 +11,7 @@ if(isset($_GET['quitter']) && $_GET['quitter']=='oui'){// on récupère le terme
 
     unset($_SESSION['connexion']);
     session_destroy();
-     header('location:connexion.php');
+    header('location:connexion.php');
 } // ferme le isset de la déconnexion
 
 if(isset ($_POST['connexion'])){// On envoie le form avec le name du button (on a cliqué dessus )
@@ -46,22 +46,49 @@ if(isset ($_POST['connexion'])){// On envoie le form avec le name du button (on 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Authentification</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style_connexion.css">
+    <link href="https://fonts.googleapis.com/css?family=Magra" rel="stylesheet">
 
 </head>
 <body>
-    <h1>Admin: s'authentifier</h1>
-    <hr>
-
+    <div class="container" id="authentify">
+        <div class="row">
+            <div class="col-md-5 col-md-offset-3">
+                <div class="alert alert-info" role="alert">
+                    <h1>Admin: s'authentifier</h1>
+                </div>
+            </div>
+        </div>
+    </div>
     <!--Formulaire de connexion à l'admin -->
+    <div class="container">
+        <div class="jumbotron">
+            <form action="connexion.php" method="post" class="form-horizontal">
+                <div class="form-group">
+                    <label for="email" class="col-sm-2 control-label">Courriel</label>
+                    <div class="col-sm-10">
+                        <input type="email" name="email" placeholder="Votre courriel"required class="form-control">
+                    </div>
+                </div>
 
-    <form action="connexion.php" method="post">
-        <label for="email">Courriel</label>
-        <input type="email" name="email" placeholder="Votre courriel"required>
-        <br>
-        <label for="mdp">Mot de passe</label>
-        <input type="password" name="mdp" placeholder="Votre mot de passe"required>
-        <br>
-        <button name="connexion" type="submit">Connexion à votre admin</button>
-    </form>
+                <div class="form-group">
+                    <label for="mdp" class="col-sm-2 control-label">Mot de passe</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="mdp" placeholder="Votre mot de passe"required class="form-control">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button name="connexion" type="submit" class="btn btn-default" >Connexion à votre admin</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
