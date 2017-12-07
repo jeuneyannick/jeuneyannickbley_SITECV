bf77f2ae7ed1948463f30b0ae763be14b1b7170a
jeune yannick <yannick.bley@lepoles.com>
2017-12-07 11:58:10 +0100

    Commit Avancement Front-End Bootstrap

--- index_bootstrap.php
+++ index_bootstrap.php
@@ -1,44 +1,117 @@
 <?php
 
-?>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="stylesheet" href="admin/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style_front.css">
-  <title>Site cv Yannick Bley</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="admin/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style_front.css">
+    <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+    <title>Site cv Yannick Bley</title>
 </head>
 <body>
-  <nav class="navbar navbar-default navbar fixed-top">
-    <div class="container">
-      <div class="col-lg-12">
-        <div class="col-md-6 col-md-offset-6">
-          <ul class="nav navbar-nav">
-            <li><a href="#"><p>Accueil</p></a></li>
-            <li><a href="#"><p>Compétences</p></a></li>
-            <li><a href="#"><p>Formations</p></a></li>
-            <li><a href="#"><p>Contact</p></a></li>
-          </ul>
+    <nav class="navbar navbar-default navbar fixed-top">
+        <div class="container">
+            <div class="col-lg-12">
+                <div class="col-md-6 col-md-offset-6">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#"><p>Accueil</p></a></li>
+                        <li><a href="#competences"><p>Compétences</p></a></li>
+                        <li><a href="#"><p>Formations</p></a></li>
+                        <li><a href="#"><p>Contact</p></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
 
-  <header class="mastehead col-lg-12">
-    <div class="container">
-      <div class="jumbotron col-md-offset-2 col-md-9">
-        <div class="intro"> Hey Bienvenue sur le site de Yannick Bley !</div>
-        <div class="description">Developpeur/Integrateur Web Junior</div>
-        <div class="search"> Je suis actuellement à la recherche d'un stage</div>
-        <a class="btn btn-danger col-md-3 col-md-offset-3" href="#"> Découvrez moi ! </a>
-      </div>
-    </header>
 
-  </body>
-  </html>
+    <main class="mastehead col-lg-12">
+        <div class="container">
+            <div class="row">
+                <div class="jumbotron text-uppercase col-md-offset-2 col-md-7">
+                    <div class="intro"> Hey Bienvenue sur le site de Yannick Bley !</div>
+                    <div class="description">Developpeur/Integrateur Web Junior</div>
+                    <div class="search"> Je suis actuellement à la recherche d'un stage</div>
+                    <a class="btn btn-danger col-md-3 col-md-offset-3 text-uppercase"  href="#"> Découvrez moi ! </a>
+                </div>
+            </div>
+        </div>
+        <section class="container">
+            <div class="row">
+                <div class="panel panel-default col-md-4">
+                    <div class="panel-heading">Competences</div>
+                    <div class="panel-body">
+                        JavaScript
+                        Css
+                        html
+                        php
+                        Bootstrap
+                    </div>
+                </div>
+                <div class="panel panel-default col-md-4 col-md-offset-4">
+                    <div class="panel-heading">Competences</div>
+                    <div class="panel-body">
+                        JavaScript
+                        Css
+                        html
+                        php
+                        Bootstrap
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="panel panel-default col-md-4">
+                    <div class="panel-heading">Competences</div>
+                    <div class="panel-body">
+                        JavaScript
+                        Css
+                        html
+                        php
+                        Bootstrap
+                    </div>
+                </div>
+                <div class="panel panel-default col-md-4 col-md-offset-4">
+                    <div class="panel-heading">Competences</div>
+                    <div class="panel-body">
+                        JavaScript
+                        Css
+                        html
+                        php
+                        Bootstrap
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="admin/js/bootstrap.min.js"></script>
+</body>
+</html>
