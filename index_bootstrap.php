8d3cdc31f1bbaa98a323b216c7421ffe9de2c24d
jeune yannick <yannick.bley@lepoles.com>
2017-12-07 16:56:49 +0100

    Commit Bootstrap

--- index_bootstrap.php
+++ index_bootstrap.php
@@ -1,20 +1,28 @@
 <?php
+require_once('admin/init/connect.php');
 
+$req = $pdo->query("SELECT* FROM t_utilisateurs WHERE id_utilisateur = '$id_utilisateur'");
+$ligne_utilisateur = $req -> fetch(PDO::FETCH_ASSOC);
 
+$req= $pdo->prepare("SELECT * FROM t_formations WHERE utilisateur_id= '$id_utilisateur'");
+$req->execute();
+$nbr_formations = $req-> rowCount();
 
+$req= $pdo->prepare("SELECT * FROM t_realisations WHERE utilisateur_id= '$id_utilisateur'");
+$req->execute();
+$nbr_realisations = $req-> rowCount();
 
+$req= $pdo->prepare("SELECT * FROM t_loisirs WHERE utilisateur_id= '$id_utilisateur'");
+$req->execute();
+$nbr_loisirs = $req-> rowCount();
 
+$req= $pdo->prepare("SELECT * FROM t_experiences WHERE utilisateur_id= '$id_utilisateur'");
+$req->execute();
+$nbr_experiences = $req-> rowCount();
 
-
-
-
-
-
-
-
-
-
-
+$req= $pdo->prepare("SELECT * FROM t_competences WHERE utilisateur_id= '$id_utilisateur'");
+$req->execute();
+$nbr_competences = $req-> rowCount();
 
 
 ?>
@@ -28,13 +36,14 @@
     <meta name="author" content="">
     <link rel="stylesheet" href="admin/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style_front.css">
+    <link rel="stylesheet" href="../font-awesome/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
     <title>Site cv Yannick Bley</title>
 </head>
 <body>
     <main class="container-fluid col-md-7 col-md-offset-2">
-        <div class="container">
-            <nav class="navbar navbar-default navbar fixed-top">
+        <nav class="navbar navbar-default navbar fixed-top">
+            <div class="container">
                 <div class="col-lg-12">
                     <div class="col-md-6 col-md-offset-6">
                         <ul class="nav navbar-nav">
@@ -45,8 +54,8 @@
                         </ul>
                     </div>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
 
 
         <section class="mastehead col-lg-12">
@@ -56,24 +65,19 @@
                         <div class="intro"> Hey Bienvenue sur le site de Yannick Bley !</div>
                         <div class="description">Developpeur/Integrateur Web Junior</div>
                         <div class="search"> Je suis actuellement à la recherche d'un stage</div>
-                        <a class="btn btn-danger col-md-3 col-md-offset-3 text-uppercase"  href="#"> Découvrez moi ! </a>
+                        <div class="btn btn-danger col-md-3 col-md-offset-3">
+                            Retrouvez moi sur
+                        </div>
                     </div>
                 </div>
             </div>
             <section class="container">
                 <div class="row">
                     <div class="panel panel-default col-md-4">
-                        <div class="panel-heading">Competences</div>
-                        <div class="panel-body">
-                            JavaScript
-                            Css
-                            html
-                            php
-                            Bootstrap
-                        </div>
+                        <button class="btn btn-danger">Competences</button>
                     </div>
-                    <div class="panel panel-default col-md-4 col-md-offset-2">
-                        <div class="panel-heading">Formations</div>
+                    <div class="panel panel-default col-md-4">
+                        <button class="btn btn-danger">Formations</button>
                         <div class="panel-body">
                             JavaScript
                             Css
@@ -85,31 +89,17 @@
                 </div>
                 <div class="row">
                     <div class="panel panel-default col-md-4">
-                        <div class="panel-heading">Experiences</div>
-                        <div class="panel-body">
-                            JavaScript
-                            Css
-                            html
-                            php
-                            Bootstrap
-                        </div>
+                        <button class="btn btn-danger">Experiences</button>
                     </div>
-                    <div class="panel panel-default col-md-4 col-md-offset-2">
-                        <div class="panel-heading">Loisirs</div>
-                        <div class="panel-body">
-                            JavaScript
-                            Css
-                            html
-                            php
-                            Bootstrap
-                        </div>
+                    <div class="panel panel-default col-md-4">
+                        <button class="btn btn-danger">
+                            Loisirs
+                        </button>
                     </div>
                 </div>
             </section>
         </section>
     </main>
-
-
 
 
 
