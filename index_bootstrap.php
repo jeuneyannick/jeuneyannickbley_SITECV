a65e8e8d53fb794c85c79dde63049183ec2f2857
Yannick <yannick.bley@lepoles.com>
2017-12-07 08:32:56 +0100

    commit bootsrap again

--- index_bootstrap.php
+++ index_bootstrap.php
@@ -6,37 +6,39 @@
 <html lang="fr">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="stylesheet" href="admin/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style_front.css">
-    <title>Site cv Yannick Bley</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="admin/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style_front.css">
+  <title>Site cv Yannick Bley</title>
 </head>
 <body>
-    <nav class="navbar navbar-default navbar fixed-top">
-        <div class="container">
-            <div class="col-lg-12">
-                <div class="col-md-12">
-                    <ul class="nav navbar-nav">
-                        <li><a href="#"><p>Accueil</p></a></li>
-                        <li><a href="#"><p>Compétences</p></a></li>
-                        <li><a href="#"><p>Formations</p></a></li>
-                        <li><a href="#"><p>Contact</p></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
-
-<header class="mastehead col-lg-12" style="background:darksalmon; width: 100%; height:100vh;">
+  <nav class="navbar navbar-default navbar fixed-top">
     <div class="container">
-        <div class="jumbotron col-md-offset-2 col-md-4">
-            <p> Hey Bienvenue sur le site de Yannick Bley !</p>
+      <div class="col-lg-12">
+        <div class="col-md-6 col-md-offset-6">
+          <ul class="nav navbar-nav">
+            <li><a href="#"><p>Accueil</p></a></li>
+            <li><a href="#"><p>Compétences</p></a></li>
+            <li><a href="#"><p>Formations</p></a></li>
+            <li><a href="#"><p>Contact</p></a></li>
+          </ul>
         </div>
+      </div>
+    </div>
+  </nav>
+
+  <header class="mastehead col-lg-12">
+    <div class="container">
+      <div class="jumbotron col-md-offset-2 col-md-9">
+        <div class="intro"> Hey Bienvenue sur le site de Yannick Bley !</div>
+        <div class="description">Developpeur/Integrateur Web Junior</div>
+        <div class="search"> Je suis actuellement à la recherche d'un stage</div>
+        <a class="btn btn-danger col-md-3 col-md-offset-3" href="#"> Découvrez moi ! </a>
+      </div>
     </header>
 
-</body>
-</html>
+  </body>
+  </html>
