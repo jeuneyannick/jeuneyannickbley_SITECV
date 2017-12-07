15b3b8573fa7bc3c6fc735d9094595aebd4b8866
jeune yannick <yannick.bley@lepoles.com>
2017-12-07 12:45:36 +0100

    commit camo bootstrap avancement

--- index_bootstrap.php
+++ index_bootstrap.php
@@ -16,6 +16,7 @@
 
 
 
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -31,81 +32,82 @@
     <title>Site cv Yannick Bley</title>
 </head>
 <body>
-    <nav class="navbar navbar-default navbar fixed-top">
-        <div class="container">
-            <div class="col-lg-12">
-                <div class="col-md-6 col-md-offset-6">
-                    <ul class="nav navbar-nav">
-                        <li><a href="#"><p>Accueil</p></a></li>
-                        <li><a href="#competences"><p>Compétences</p></a></li>
-                        <li><a href="#"><p>Formations</p></a></li>
-                        <li><a href="#"><p>Contact</p></a></li>
-                    </ul>
+    <main class="container-fluid">
+        <nav class="navbar navbar-default navbar fixed-top">
+            <div class="container">
+                <div class="col-lg-12">
+                    <div class="col-md-6 col-md-offset-6">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#index_bootstrap.php"><p>Accueil</p></a></li>
+                            <li><a href="#competences"><p>Compétences</p></a></li>
+                            <li><a href="#Formations"><p>Formations</p></a></li>
+                            <li><a href="#Contact"><p>Contact</p></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
 
-    <main class="mastehead col-lg-12">
-        <div class="container">
-            <div class="row">
-                <div class="jumbotron text-uppercase col-md-offset-2 col-md-7">
-                    <div class="intro"> Hey Bienvenue sur le site de Yannick Bley !</div>
-                    <div class="description">Developpeur/Integrateur Web Junior</div>
-                    <div class="search"> Je suis actuellement à la recherche d'un stage</div>
-                    <a class="btn btn-danger col-md-3 col-md-offset-3 text-uppercase"  href="#"> Découvrez moi ! </a>
-                </div>
-            </div>
-        </div>
-        <section class="container">
-            <div class="row">
-                <div class="panel panel-default col-md-4">
-                    <div class="panel-heading">Competences</div>
-                    <div class="panel-body">
-                        JavaScript
-                        Css
-                        html
-                        php
-                        Bootstrap
-                    </div>
-                </div>
-                <div class="panel panel-default col-md-4 col-md-offset-4">
-                    <div class="panel-heading">Competences</div>
-                    <div class="panel-body">
-                        JavaScript
-                        Css
-                        html
-                        php
-                        Bootstrap
+        <section class="mastehead col-lg-12">
+            <div class="container">
+                <div class="row">
+                    <div class="jumbotron text-uppercase col-md-offset-2 col-md-7">
+                        <div class="intro"> Hey Bienvenue sur le site de Yannick Bley !</div>
+                        <div class="description">Developpeur/Integrateur Web Junior</div>
+                        <div class="search"> Je suis actuellement à la recherche d'un stage</div>
+                        <a class="btn btn-danger col-md-3 col-md-offset-3 text-uppercase"  href="#"> Découvrez moi ! </a>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="panel panel-default col-md-4">
-                    <div class="panel-heading">Competences</div>
-                    <div class="panel-body">
-                        JavaScript
-                        Css
-                        html
-                        php
-                        Bootstrap
+            <section class="container">
+                <div class="row">
+                    <div class="panel panel-default col-md-4">
+                        <div class="panel-heading">Competences</div>
+                        <div class="panel-body">
+                            JavaScript
+                            Css
+                            html
+                            php
+                            Bootstrap
+                        </div>
+                    </div>
+                    <div class="panel panel-default col-md-4 col-md-offset-4">
+                        <div class="panel-heading">Formations</div>
+                        <div class="panel-body">
+                            JavaScript
+                            Css
+                            html
+                            php
+                            Bootstrap
+                        </div>
                     </div>
                 </div>
-                <div class="panel panel-default col-md-4 col-md-offset-4">
-                    <div class="panel-heading">Competences</div>
-                    <div class="panel-body">
-                        JavaScript
-                        Css
-                        html
-                        php
-                        Bootstrap
+                <div class="row">
+                    <div class="panel panel-default col-md-4">
+                        <div class="panel-heading">Experiences</div>
+                        <div class="panel-body">
+                            JavaScript
+                            Css
+                            html
+                            php
+                            Bootstrap
+                        </div>
+                    </div>
+                    <div class="panel panel-default col-md-4 col-md-offset-4">
+                        <div class="panel-heading">Loisirs</div>
+                        <div class="panel-body">
+                            JavaScript
+                            Css
+                            html
+                            php
+                            Bootstrap
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </section>
     </main>
-
 
 
 
