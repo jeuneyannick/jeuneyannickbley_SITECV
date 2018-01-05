52b178d48eb65d5689a1d275a82c220e81242202
Yannick Bley <yannick.bley@lepoles.com>
2018-01-05 08:33:02 +0100

    Commit prepa front-end

--- mobilefirst.php
+++ mobilefirst.php
@@ -42,6 +42,7 @@ $affiche_competences = $req-> fetchAll(PDO::FETCH_ASSOC);
   <title>Site cv Yannick Bley</title>
 </head>
 <body>
+
   <!--Page d'accueil -->
   <div class="first container">
 
@@ -50,17 +51,17 @@ $affiche_competences = $req-> fetchAll(PDO::FETCH_ASSOC);
       <div class="row">
         <nav class="text-center navbar navbar-inverse col-xs-12 col-sm-10 col-lg-7 col-lg-push-3" style="background: #eee;">
           <ul class=" nav navbar-nav list-inline" style="text-align: center; padding-top:2px; ">
-            <li><a href="#Exo_bootstrap.html">Accueil</a></li>
+            <li><a href="#mobilefirst.php">Accueil</a></li>
             <li><a href="#Competences">Compétences</a></li>
             <li><a href="#Formations">Formations</a></li>
             <li><a href="#Experiences">Expériences</a></li>
-            <li><a href="#Realisations">Réalisations</a></li>
+            <!-- <li><a href="#Realisations">Réalisations</a></li> -->
             <li><a href="#Contact">Contact</a></li>
           </ul>
         </nav>
       </div>
-    </header>
-    <!-- Fin du menu !-->
+    </header><!-- Fin du menu !-->
+
 
     <!--Section de présentation du site -->
     <section class="jumbotron col-lg-7 col-sm-7 col-xs-12 ">
@@ -82,8 +83,8 @@ $affiche_competences = $req-> fetchAll(PDO::FETCH_ASSOC);
       </div>
 
 
-    </section>
-    <!--Fin de la section !-->
+    </section><!--Fin de la section !-->
+
     <!-- Section à propos -->
     <section class="panel panel-default col-lg-4 col-lg-offset-1 col-sm-4 col-sm-offset-1 col-xs-12">
       <div class="panel panel-heading">
@@ -93,139 +94,110 @@ $affiche_competences = $req-> fetchAll(PDO::FETCH_ASSOC);
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
 
-    </section>
-    <!--Fin de la section -->
-  </div>
-  <!-- Fin de la page d'accueil -->
+    </section><!--Fin de la section -->
+  </div><!-- Fin de la page d'accueil -->
 
-  <!--Début page Compétences -->
-  <div class="second container" id="Competences">
+
+  <div class="second container" id="Competences"> <!-- Début page Competences -->
 
     <div class="row">
       <div class="page-header text-center col-lg-6 col-lg-push-3 col-sm-6 col-sm-push-3 col-md-6 col-md-push-3 col-xs-12">
         <strong>Compétences</strong>
       </div>
     </div>
+
     <div class="container jumbotron">
       <div class="row">
-        <div class="competence col-lg-4 col-lg-push-4 col-md-4 col-md-push-4 col-sm-4 col-sm-push-4 col-xs-12">
+        <article class="competence col-lg-4 col-md-4 col-sm-4 col-xs-12">
           <p class="text-center">Competences</p>
           <?php foreach($affiche_competences as $competence){?>
             <div class="text-center col-lg-3 col-md-3 col-sm-5 col-xs-12"> <?= $competence['competence']?></div>
-            <div class="text-center col-lg-3 col-md-3 col-sm-5 col-xs-12"> <?= $competence['c_niveau']?> </div>
+
           <?php } ?>
-        </div>
+        </article>
       </div>
     </div>
-  </div>
-  <!--Fin du cadre Jumbotron -->
-</div>
-<!-- Fin du corps de l'encadrement-->
+  </div><!--Fin de la page Compétences -->
 
 
+  <div class="clearfix"></div>
 
-
-</div>
-
-
-</div>
-</div>
-<!--Fin de la page Compétences -->
-
-<div class="clearfix">
-</div>
-<!-- Début de la page Formations -->
-<div class="third container" id="Formations">
-  <div class="row">
-
-    <div class="page-header text-center col-lg-6 col-lg-push-3 col-sm-6 col-sm-push-3 col-md-6 col-md-push-3 col-xs-12">
-      <strong>Formations</strong>
-    </div>
-
-  </div>
-
-  <div class="row text-center">
-    <div class="col-lg-3 col-lg-push-1 col-md-3 col-md-push-1 col-sm-3 col-sm-push-1 col-xs-12">
-      <div class="cadre" style=" width: 300px; height: 300px; border-radius: 200px; background:indianred;">
-        <div class="icons text-center">
-          <i class="fa fa-graduation-cap fa-4x text-center" aria-hidden="true"></i>
-        </div>
-        <div class="description">
-          <?php foreach($affiche_formations as $affiche_formation){?>
-            <div><?= $affiche_formation['']?></div>
-            <div><?= ?> </div>
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Fin de la page Formations -->
-
-<div class="fourth container" id="Experiences">
-  <div class="row">
-    <div class="page-header text-center col-lg-6 col-lg-push-3 col-sm-6 col-sm-push-3 col-md-6 col-md-push-3 col-xs-12">
-      <strong>Expériences</strong>
-    </div>
-  </div>
-  <div class="container">
+  <div class="third container" id="Formations"><!-- Début de la page Formations -->
     <div class="row">
-      <div class="col-lg-3 col-lg-push-1  col-md-3 col-md-push-1  col-sm-3 col-sm-push-1  col-xs-12">
-        <div class="cadre text-center" style=" width: 200px; height: 200px; border-radius: 100px; background:indianred;">
-          <div class="icons text-center">
-            <i class="fa fa-graduation-cap fa-4x text-center" aria-hidden="true"></i>
-          </div>
-          <div class="description">
-            <p>Baccalauréat Série littéraire</p>
-            <p>Lycée Michel-Ange,Villeneuve-la-Garenne</p>
-            <p>2008-2009</p>
-          </div>
-        </div>
+
+      <div class="page-header text-center col-lg-6 col-lg-push-3 col-sm-6 col-sm-push-3 col-md-6 col-md-push-3 col-xs-12">
+        <strong>Formations</strong>
       </div>
-      <div class="col-lg-3 col-lg-push-2 col-md-3 col-lg-push-1  col-sm-3 col-sm-push-1  col-xs-12">
-        <div class="cadre text-center" style=" width: 200px; height: 200px; border-radius: 100px; background:indianred;">
-          <div class="icons text-center">
-            <i class="fa fa-graduation-cap fa-4x text-center" aria-hidden="true"></i>
-          </div>
-          <div class="description">
-            <p>Baccalauréat Série littéraire</p>
-            <p>Lycée Michel-Ange,Villeneuve-la-Garenne</p>
-            <p>2008-2009</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-lg-push-2 col-md-3 col-md-push-1 col-sm-3 col-sm-push-1  col-xs-12">
-        <div class="cadre text-center" style=" width: 200px; height: 200px; border-radius: 100px; background:indianred;">
-          <div class="icons text-center">
-            <i class="fa fa-graduation-cap fa-4x text-center" aria-hidden="true"></i>
-          </div>
-          <div class="description">
-            <p>Baccalauréat Série littéraire</p>
-            <p>Lycée Michel-Ange,Villeneuve-la-Garenne</p>
-            <p>2008-2009</p>
-          </div>
-        </div>
+
+    </div>
+    <div class="container">
+      <div class="row text-center">
+        <?php foreach($affiche_formations as $affiche_form){?>
+          <article class="col-lg-3  col-md-3  col-sm-3 col-xs-12">
+            <div class="cadre" style=" width: 300px; height: 300px; border-radius: 200px; background:indianred;">
+
+              <div class="icons text-center">
+                <i class="fa fa-graduation-cap fa-4x text-center" aria-hidden="true"></i>
+              </div>
+
+              <div class="description">
+                <div><?= $affiche_form['f_titre']?></div>
+                <div><?= $affiche_form['f_lieu']?></div>
+                <div><?= $affiche_form['f_dates']?></div>
+                <div><?= $affiche_form['f_description']?></div>
+              </div>
+
+            </div>
+          </article>
+        <?php } ?>
       </div>
     </div>
-  </div>
+  </div><!-- Fin de la page Formations -->
 
-</div>
+  <div class="clearfix"></div>
+
+  <div class="fourth container" id="Experiences"><!-- Début de la page Experiences -->
+    <div class="row">
+      <div class="page-header text-center col-lg-6 col-lg-push-3 col-sm-6 col-sm-push-3 col-md-6 col-md-push-3 col-xs-12">
+        <strong>Expériences</strong>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <?php foreach($affiche_experiences as $affiche_exp){?>
+          <div class="col-lg-3 col-lg-push-1  col-md-3 col-md-push-1  col-sm-3 col-sm-push-1  col-xs-12">
+            <div class="cadre text-center" style=" width: 200px; height: 200px; border-radius: 100px; background:indianred;">
+              <div class="icons text-center">
+                <i class="fa fa-graduation-cap fa-4x text-center" aria-hidden="true"></i>
+              </div>
+
+              <div><?= $affiche_exp['e_poste']?></div>
+              <div><?= $affiche_exp['e_employeur']?></div>
+              <div><?= $affiche_exp['e_lieu']?></div>
+              <div><?= $affiche_exp['e_dates']?></div>
+              <div><?= $affiche_exp['e_description']?></div>
+            <?php } ?>
+
+          </div>
+        </div>
+      </div>
 
 
-<footer class="text-center">
-  <p>Tous droits reservés</p>
-</footer>
+    </div>
+  </div><!-- Fin de la page Expériences -->
 
-<<<<<<< HEAD
-sdsqddsqd
-=======
-fefefEZfez
->>>>>>> c2ac0347461e385fdfa7df68a04fb88243289185
+  <div class="clearfix"></div>
 
 
+  <footer class="text-center">
+    <p> <?= date('Y'); ?> -  Tous droits reservés</p>
+  </footer>
 
-<script src="admin/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="admin/js/jquery-3.2.1.min.js"></script>
-<script src="admin/js/mobilefirst.js"></script>
+
+
+  <script src="admin/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="admin/js/jquery-3.2.1.min.js"></script>
+  <script src="admin/js/mobilefirst.js"></script>
 </body>
